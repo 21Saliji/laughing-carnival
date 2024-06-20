f67bc484f13d637171a3c26d65f488b26383e628
Yankho Chantel Maria Saliji <ysaliji023@student.wethinkcode.co.za>
2024-06-20 18:37:09 +0200

    created a main function

--- String_manipulation_cipher/string_cipher.py
+++ String_manipulation_cipher/string_cipher.py
@@ -1,51 +1,58 @@
- ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 def get_clean_message(text):
-  """
-  Removes non-alphabetic characters from the message.
-  """
-  cleaned_message = ''.join(char for char in text.lower() if char.isalpha())
-  return cleaned_message
+    """
+    Removes non-alphabetic characters from the message.
+    """
+    cleaned_message = ''.join(char for char in text.lower() if char.isalpha())
+    return cleaned_message
 
 def generate_key(message, key):
-  """
-  Repeats the key to match the length of the message.
-  """
-  repeated_key = key * (len(message) // len(key) + 1)
-  return repeated_key[:len(message)]
+    """
+    Repeats the key to match the length of the message.
+    """
+    repeated_key = key * (len(message) // len(key) + 1)
+    return repeated_key[:len(message)]
 
 def caesar_cipher(message, key, direction=1):
-  """
-  Performs Caesar cipher with the given key and direction (encrypt/decrypt).
-  """
-  cleaned_message = get_clean_message(message)
-  repeated_key = generate_key(cleaned_message, key)
-  final_message = ''
+    """
+    Performs Caesar cipher with the given key and direction (encrypt/decrypt).
+    """
+    cleaned_message = get_clean_message(message)
+    repeated_key = generate_key(cleaned_message, key)
+    final_message = ''
 
-  for char, key_char in zip(cleaned_message, repeated_key):
-    offset = ALPHABET.index(key_char)
-    index = ALPHABET.find(char)
-    new_index = (index + offset * direction) % len(ALPHABET)
-    final_message += ALPHABET[new_index]
+    for char, key_char in zip(cleaned_message, repeated_key):
+        offset = ALPHABET.index(key_char)
+        index = ALPHABET.find(char)
+        new_index = (index + offset * direction) % len(ALPHABET)
+        final_message += ALPHABET[new_index]
 
-  return final_message
+    return final_message
 
 def encrypt(message, key):
-  """
-  Encrypts the message using Caesar cipher.
-  """
-  return caesar_cipher(message, key)
+    """
+    Encrypts the message using Caesar cipher.
+    """
+    return caesar_cipher(message, key)
 
 def decrypt(message, key):
-  """
-  Decrypts the message using Caesar cipher.
-  """
-  return caesar_cipher(message, key, -1)
+    """
+    Decrypts the message using Caesar cipher.
+    """
+    return caesar_cipher(message, key, -1)
 
-text = 'mrttaqrhknsw ih puggrur'
-custom_key = 'python'
+def main():
+    text = 'mrttaqrhknsw ih puggrur'
+    custom_key = 'python'
 
-print(f'\nEncrypted text: {encrypt(text, custom_key)}')
-print(f'Key: {custom_key}')
-decryption = decrypt(text, custom_key)
-print(f'\nDecrypted text: {decryption}\n')
+    encrypted_text = encrypt(text, custom_key)
+    print(f'Original text: {text}')
+    print(f'Encrypted text: {encrypted_text}')
+    print(f'Key used: {custom_key}')
+
+    decrypted_text = decrypt(encrypted_text, custom_key)
+    print(f'Decrypted text: {decrypted_text}')
+
+if __name__ == "__main__":
+    main()
